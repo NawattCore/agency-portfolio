0ef9aacbc9546b102b8fee8667a4b423d0392a0e
mohamed hassan mohamed <mh3073629@gmail.com>
2025-09-14 23:22:34 +0300

    try deploy

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,6 +1,6 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
-import HeroSlider from '../../components/Hero'
+import Hero from '../../components/Hero'
 
 export default function Home() {
   return (
